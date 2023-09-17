5e6296b80c3d919189f461518fc2e324c1785ff9
aries grey <ariesgrey88@gmail.com>
2023-09-17 14:20:41 -0500

    notFound, errorHandler created + app.js config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
+app.use(cors());
+app.use(express.json());
+
+// Not found handler
+app.use(notFound);
+
+// Error handler
+app.use(errorHandler);
 
 module.exports = app;
